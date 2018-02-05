7e5887a5061a382df6285f540fafb0278b4789a6
nikhil15iitd <nikhil15iitd@gmail.com>
2018-02-04 19:34:35 -0500

    Code that first trains feed forward network, performs poorly

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import numpy as np
 import json
 from collections import OrderedDict
-from nltk.corpus import stopwords, wordnet as wn, words
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from keras.layers import Bidirectional, LSTM, Conv1D, Dense, PReLU, Dropout, Input, Activation, TimeDistributed
+from keras.layers import Bidirectional, LSTM, Conv1D, Dense, PReLU, MaxPool1D, Input, add, TimeDistributed
 from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
 
 # Global variables for length of inputs & outputs
 PROBLEM_LENGTH = 105
 TEMPLATE_LENGTH = 30
+SEED = 23
 
 stop_words = set(stopwords.words('english'))
 vocab = OrderedDict()
@@ -17,27 +19,32 @@ vocab = OrderedDict()
 operators = {'+': 1, '-': 2, '*': 3, '/': 4, '=': 5}
 knowns = {'a': 6, 'b': 7, 'c': 8, 'd': 9, 'e': 10, 'f': 11, 'g': 12}
 unknowns = {'m': 13, 'n': 14, 'l': 15, 'o': 16, 'p': 17, 'q': 18}
-
-'''
-Function to map network input (which is numbers back to word problem)
-'''
+separators = {',': 20}
+all_template_vars = {0: ' ', 20: ','}
 
 
 def numbers_to_words(num_vector):
+    '''
+    Function to map network input (which is numbers back to word problem)
+    '''
     keys = list(vocab.keys())
     return [keys[i - 1] for i in num_vector]
 
 
-'''
-Function to map network output (which is numbers back to template equation)
-'''
-
-
 def derivation_to_equation(num_vector):
-    pass
+    '''
+    Function to map network output (which is numbers back to template equation)
+    '''
+    return [all_template_vars[int(i)] for i in num_vector]
 
 
 def read_draw():
+    for key in operators.keys():
+        all_template_vars[operators[key]] = key
+    for key in unknowns.keys():
+        all_template_vars[unknowns[key]] = key
+    for key in knowns.keys():
+        all_template_vars[knowns[key]] = key
     X = []
     Y = []
     max_len = -10
@@ -80,20 +87,41 @@ def read_draw():
 def pad_lengths_to_constant(X, Y):
     newX = pad_sequences(X, padding='post', truncating='post', value=0., maxlen=PROBLEM_LENGTH)
     newY = pad_sequences(Y, padding='post', truncating='post', value=0., maxlen=TEMPLATE_LENGTH)
+    newX = np.reshape(newX, (-1, PROBLEM_LENGTH, 1))
+    newY = np.reshape(newY, (-1, TEMPLATE_LENGTH, 1))
     return newX, newY
 
 
-'''
-Deep neural network model to get F(x) which is to be fed to SPENs
-'''
-
-
-def model(input_shape):
+def feed_forward_model(input_shape):
+    '''
+    Deep neural network model to get F(x) which is to be fed to SPENs
+    '''
     inputs = Input(shape=input_shape)
     l0 = Bidirectional(LSTM(32, return_sequences=True))(inputs)
     l0 = Bidirectional(LSTM(32, return_sequences=True))(l0)
+    l0 = Conv1D(64, 5)(l0)
+    l0 = PReLU()(l0)
+    l0 = Conv1D(64, 5)(l0)
+    l0 = PReLU()(l0)
+    l0 = MaxPool1D(3)(l0)
+    l0 = Conv1D(128, 3)(l0)
+    l0 = PReLU()(l0)
+    l0 = TimeDistributed(Dense(1, activation='relu'))(l0)
     model = Model(inputs=inputs, outputs=l0)
+    model.compile('adam', 'mse', metrics=['acc'])
+    return model
 
 
 def main():
-    X, Y = pad_lengths_to_constant(read_draw())
+    X, Y = read_draw()
+    X, Y = pad_lengths_to_constant(X, Y)
+    F = feed_forward_model(X.shape[1:])
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=SEED)
+    F.fit(X_train, y_train, batch_size=64, epochs=10, validation_data=(X_test, y_test))
+    y_pred = F.predict(X_test)
+    for i in range(y_pred.shape[0]):
+        print(derivation_to_equation(y_pred[i].reshape((TEMPLATE_LENGTH,))))
+        print(derivation_to_equation(y_test[i].reshape((TEMPLATE_LENGTH,))))
+
+
+main()
