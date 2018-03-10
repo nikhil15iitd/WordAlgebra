9bf6ac8dd110866eaf3cdfb1f1373ba2c838e36e
pentiumx <ggando@umass.edu>
2018-03-10 17:07:22 -0500

    Remove copied modules and import them instead in train_mlp.py

--- train_mlp.py
+++ train_mlp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json
+
 from collections import OrderedDict
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
@@ -9,113 +10,12 @@ from keras.layers import Bidirectional, LSTM, Conv1D, Dense, PReLU, MaxPool1D, I
 from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
+
+import config_wordalgebra
+from dataset import read_draw, numbers_to_words, derivation_to_equation
 from template_parser import debug
 
-# Global variables for length of inputs & outputs
-PROBLEM_LENGTH = 105
-TEMPLATE_LENGTH = 30
-SEED = 23
 
-stop_words = set(stopwords.words('english'))
-vocab = OrderedDict()
-# nouns = {x.name().split('.', 1)[0] for x in wn.all_synsets('n')}
-operators = {'+': 1, '-': 2, '*': 3, '/': 4, '=': 5}
-knowns = {'a': 6, 'b': 7, 'c': 8, 'd': 9, 'e': 10, 'f': 11, 'g': 12}
-unknowns = {'m': 13, 'n': 14, 'l': 15, 'o': 16, 'p': 17, 'q': 18}
-symbols = {'%': 19}
-separators = {',': 20}
-all_template_vars = {0: ' ', 20: ','}
-
-
-def numbers_to_words(num_vector):
-    '''
-    Function to map network input (which is numbers back to word problem)
-    '''
-    keys = list(vocab.keys())
-    return [keys[i - 1] for i in num_vector]
-
-
-def derivation_to_equation(num_vector):
-    '''
-    Function to map network output (which is numbers back to template equation)
-    '''
-    return [all_template_vars[int(i)] for i in num_vector]
-
-
-def read_draw():
-    for key in operators.keys():
-        all_template_vars[operators[key]] = key
-    for key in unknowns.keys():
-        all_template_vars[unknowns[key]] = key
-    for key in knowns.keys():
-        all_template_vars[knowns[key]] = key
-    for key in symbols.keys():
-        all_template_vars[symbols[key]] = key
-    X = []
-    Y = []
-    max_len = -10
-    with open('0.7 - release/draw.json', 'r') as f:
-        datastore = json.load(f)
-        for questions in datastore:
-            x = []
-            y = []
-            # process each question
-            # split into sentences
-            sentences = questions['sQuestion'].split('.')
-            for sentence in sentences:
-                word_tokens = word_tokenize(sentence)
-                for word in word_tokens:
-                    if word not in vocab:
-                        vocab[word] = len(vocab) + 1
-                    x.append(vocab[word])
-                x.append(20)
-            for template in questions['Template']:
-                for slot in template.split(' '):
-                    if slot in knowns:
-                        y.append(knowns[slot])
-                    elif slot in unknowns:
-                        y.append(unknowns[slot])
-                    elif slot in operators:
-                        y.append(operators[slot])
-                    else:
-                        y.append(0)
-                y.append(20)
-            # print(y)
-            # print(x)
-            if max_len < len(x):
-                max_len = len(x)
-            X.append(x)
-            Y.append(y)
-    print('Max length: ' + str(max_len))
-    return X, Y
-
-
-def pad_lengths_to_constant(X, Y):
-    newX = pad_sequences(X, padding='post', truncating='post', value=0., maxlen=PROBLEM_LENGTH)
-    newY = pad_sequences(Y, padding='post', truncating='post', value=0., maxlen=TEMPLATE_LENGTH)
-    newY = np.reshape(newY, (-1, TEMPLATE_LENGTH, 1))
-    return newX, newY
-
-
-def feed_forward_model(input_shape, vocab_size, template_vocab_size):
-    '''
-    Deep neural network model to get F(x) which is to be fed to SPENs
-    '''
-    inputs = Input(shape=input_shape)
-    l0 = Embedding(vocab_size, 16, input_length=PROBLEM_LENGTH)(inputs)
-    l0 = Bidirectional(LSTM(32, return_sequences=True))(l0)
-    l0 = Bidirectional(LSTM(32, return_sequences=True))(l0)
-    l0 = Conv1D(64, 5)(l0)
-    l0 = PReLU()(l0)
-    l0 = Conv1D(64, 5)(l0)
-    l0 = PReLU()(l0)
-    l0 = MaxPool1D(3)(l0)
-    l0 = Conv1D(128, 3)(l0)
-    l0 = PReLU()(l0)
-    l0 = TimeDistributed(Dense(template_vocab_size, activation='softmax'))(l0)
-    model = Model(inputs=inputs, outputs=l0)
-    model.compile('adam', 'sparse_categorical_crossentropy', metrics=['acc'])
-    return model
 
 
 def feed_forward_rnn_model(input_shape, vocab_size):
@@ -258,6 +158,39 @@ def feed_forward_mlp_model(batch_size, input_shape, vocab_size):
     return model
 
 
+def feed_forward_mlp_model_coeffs(batch_size, input_shape, vocab_size):
+    '''
+    Deep neural network model to get F(x) which is to be fed to SPENs
+    '''
+    print(input_shape) # 105,
+    print(vocab_size) # => 183
+    print(keras.__version__)
+    num_output = 7 # since the template vector has 7 elems (without unknowns)
+
+    inputs = Input(shape=input_shape)
+    emb = Embedding(vocab_size, 16, input_length=config_wordalgebra.PROBLEM_LENGTH)(inputs) # => (?, 105, 16)
+
+    l0 = keras.layers.GRU(32, return_sequences=False)(emb) # => (?, 32)
+    print(type(l0)) # => <class 'tensorflow.python.framework.ops.Tensor'>
+
+    # 9 Dense layers for predicting word index in each slot
+    #l0 = Dense(9, activation=custom_softmax)(l0)
+    outputs = []
+    for i in range(num_output):
+        if i == 0:
+            outputs.append( Dense(230, activation='softmax')(l0) ) # template
+        elif i > 0 and i < 8:
+            outputs.append( Dense(config_wordalgebra.PROBLEM_LENGTH, activation='softmax')(l0) ) # coeffs
+    print('output shape:')
+    print(outputs[0].shape) # => batch_size x 9
+
+    model = Model(inputs=inputs, outputs=outputs)
+    optimizer = keras.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(optimizer, 'sparse_categorical_crossentropy', metrics=['acc'])
+
+    return model
+
+
 
 def get_layers():
     layers = [(1000, 'relu')]
@@ -266,58 +199,40 @@ def get_layers():
 
 
 def main():
-    (X, Y), vocab_dataset = debug()
+    #(X, Y), vocab_dataset = debug()
+    X, Y, vocab_dataset = debug()
     print('#'*100)
     print(X[0])
-    X = pad_sequences(X, padding='post', truncating='post', value=0., maxlen=PROBLEM_LENGTH)
+    X = pad_sequences(X, padding='post', truncating='post', value=0., maxlen=config_wordalgebra.PROBLEM_LENGTH)
     print('#'*100)
     print(Y)
     Y = np.array(Y)
 
     batch_size = 128
-    F = feed_forward_mlp_model(batch_size, X.shape[1:], len(vocab_dataset.keys()))
+    F = feed_forward_mlp_model_coeffs(batch_size, X.shape[1:], len(vocab_dataset.keys()))
     F.summary()
     # X, Y = read_draw()
     # X, Y = pad_lengths_to_constant(X, Y)
     # F = feed_forward_model(X.shape[1:], len(vocab.keys()), len(all_template_vars.keys()))
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=SEED)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=config_wordalgebra.SEED)
     ntrain = X_train.shape[0]
+    print(X_train.shape)
+    print(X_test.shape)
 
 
-    # (convert y to one-hot in the case of categorical losses)
-    #y_train = keras.utils.to_categorical(y_train)
-    #y_test = keras.utils.to_categorical(y_test)
-    #y_train = y_train.reshape(-1, 9, 1)
-    #y_test = y_test.reshape(-1, 9, 1)
+    template_size = 7
 
-    #F.fit(X_train, y_train, batch_size=batch_size, epochs=30, validation_data=(X_test, y_test))
     targets = []
-    for i in range(9):
-        #targets.append(y_train[:,i].reshape(-1))
+    for i in range(template_size):
         targets.append(y_train[:,i])
 
     test_targets = []
-    for i in range(9):
+    for i in range(template_size):
         test_targets.append(y_test[:,i])
 
-    print(y_train[:,0].shape)
-    print(type(targets))
-    print(type(y_train[:,0]))
-    #targets = np.array(targets)
-    #test_targets = np.array(test_targets)
-    #print(targets.shape)
-    #print(test_targets.shape)
 
-    F.fit(X_train, targets, batch_size=batch_size, epochs=10, validation_data=(X_test, test_targets))
-    #F.fit(X_train, [ np.array(y_train[:,0]), np.array(y_train[:,1]) ], batch_size=batch_size, epochs=1, validation_data=(X_test, y_test))
-    #F.fit(X_train, [ np.array(np.random.random((8,1))), np.array(np.random.random((8,1))) ], batch_size=batch_size, epochs=1, validation_data=(X_test, y_test))
-    #F.fit(X_train, [ np.array([0,0,0,0,0,0,0,0]), np.array([0,0,0,0,0,0,0,0]) ], batch_size=batch_size, epochs=1, validation_data=(X_test, [ np.array([0,0]), np.array([0,0]) ]))
-    #F.fit(X_train, [ targets[0], targets[1], targets[2], targets[3], targets[4], targets[5], targets[6], targets[7], targets[8] ],
-        #batch_size=batch_size, epochs=30, validation_data=(X_test, y_test))
-
-    #y_pred = np.argmax(F.predict(X_test), axis=2)
-    #y_pred_train = F.predict(X_test) # => a list of 9 elems because the model returns 9 outputs
-    #y_pred_test = F.predict(X_test)
+    F.fit(X_train, targets, batch_size=batch_size, epochs=100, validation_data=(X_test, test_targets))
+    F.save('baseline_debug.h5')
     print('='*100)
     print(F.predict(X_test)[0].shape) # 2, 230
     print(F.predict(X_test)[1].shape) # 2, 183
@@ -328,13 +243,25 @@ def main():
         tmp = np.argmax(out, axis=1)
         preds.append(tmp)
     preds = np.array(preds)
-    # Convert the output back to (N x 9)
-    preds = preds.reshape(-1, 9)
+    # Convert the output back to (N x temlpate_size)
+    preds = preds.reshape(-1, template_size)
     print(preds.shape)
     y_pred_test = preds
 
     for i in range(y_pred_test.shape[0]):
-        print('#'*100)
+        print('='*100)
+        #print(derivation_to_equation(y_pred[i].reshape((TEMPLATE_LENGTH,))))
+        #print(derivation_to_equation(y_test[i].reshape((TEMPLATE_LENGTH,))))
+        #print(derivation_to_equation(y_pred[i]))
+        #print(derivation_to_equation(y_test[i]))
+        print(y_pred_test[i])
+        print(y_test[i])
+
+    print('#'*100)
+    print('#'*100)
+
+    for i in range(y_pred_test.shape[0]):
+        print('='*100)
         #print(derivation_to_equation(y_pred[i].reshape((TEMPLATE_LENGTH,))))
         #print(derivation_to_equation(y_test[i].reshape((TEMPLATE_LENGTH,))))
         #print(derivation_to_equation(y_pred[i]))
@@ -343,6 +270,6 @@ def main():
         print(y_test[i])
 
 
-
-
-main()
+if __name__ == "__main__":
+    config_wordalgebra.init()
+    main()
