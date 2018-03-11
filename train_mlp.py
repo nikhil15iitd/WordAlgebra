2f1b94dbe95251b4b62ced93e7155113ccdae745
pentiumx <ggando@umass.edu>
2018-03-11 17:06:55 -0400

    Add a batch logger
    ref: https://github.com/keras-team/keras/issues/2850

--- train_mlp.py
+++ train_mlp.py
@@ -15,6 +15,35 @@ import globals
 from dataset import read_draw, numbers_to_words, derivation_to_equation
 from template_parser import debug
 
+class NBatchLogger(keras.callbacks.Callback):
+    """
+    A Logger that log average performance per `display` steps.
+    ref: https://github.com/keras-team/keras/issues/2850
+    """
+    def __init__(self, display):
+        self.step = 0
+        self.display = display
+        self.metric_cache = {}
+
+    def on_batch_end(self, batch, logs={}):
+        self.step += 1
+        for k in self.params['metrics']:
+            if k in logs:
+                self.metric_cache[k] = self.metric_cache.get(k, 0) + logs[k]
+        if self.step % self.display == 0:
+            metrics_log = ''
+            for (k, v) in self.metric_cache.items():
+                val = v / self.display
+                if abs(val) > 1e-3:
+                    metrics_log += ' - %s: %.4f' % (k, val)
+                else:
+                    metrics_log += ' - %s: %.4e' % (k, val)
+            print('step: {}/{} ... {}'.format(self.step,
+                                          self.params['steps'],
+                                          metrics_log))
+            self.metric_cache.clear()
+
+
 def load_glove(vocab):
     # ref: https://blog.keras.io/using-pre-trained-word-embeddings-in-a-keras-model.html
     GLOVE_DIR = 'glove.6B'
@@ -300,6 +329,8 @@ def main():
 
     #F.fit(X_train, targets, batch_size=batch_size, epochs=100, validation_data=(X_test, test_targets))
     F.fit(X_train, y_train, batch_size=batch_size, epochs=1000, validation_data=(X_test, y_test))
+    #out_batch = NBatchLogger(display=10) # show every 10 batches
+    #F.fit(X_train, y_train, batch_size=batch_size, epochs=100, validation_data=(X_test, y_test), callbacks=[out_batch], verbose=0)
     F.save('baseline_debug.h5')
     print('='*100)
     print(F.predict(X_test)[0].shape) # 2, 230
